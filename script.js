7277b512381afc4d38101e05a6e5d745a49f8d4c
Grenish rai <ariestheracer@gmail.com>
2022-12-20 19:39:18 +0530

    updated the image slider with javascript and added the cards for the content

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const indexLink = document.getElementsByClassName('indexLink')[0]
 toggleButton.addEventListener('click', ()=>{
     indexLink.classList.toggle('active')
 })
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked=true;
+    counter++;
+    if (counter > 4){
+        counter = 1;
+    }
+}, 5000);
